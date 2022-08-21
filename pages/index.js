f6e7a6c5cdac6e99900d813275b939e78fb8bdd2
Rifqi Fadhil <rifqife@gmail.com>
2022-08-21 22:27:59 +0700

    redirect from homepage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 // Components
 import Master from "~/components/Master";
 
+// Dependencies
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 function Index() {
-	return <Master></Master>;
+	const router = useRouter();
+
+	useEffect(() => {
+		router.push("/daftar");
+	}, []);
+
+	return <div>Anda akan diarahkan ...</div>;
 }
 
 export default Index;
